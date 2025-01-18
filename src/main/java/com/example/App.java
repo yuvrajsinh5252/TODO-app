8e880cb04c381d497c0d18ae542587e300918e46
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2025-01-18 15:37:36 +0530

    chore: update

--- src/main/java/com/example/App.java
+++ src/main/java/com/example/App.java
@@ -1,7 +1,27 @@
 package com.example;
 
-public class App extends MainController {
-    public static void main(String[] args) {
-        launch(args);
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class App extends Application {
+    @Override
+    public void start(Stage stage) {
+        Parent root;
+        try {
+            root = FXMLLoader.load(getClass().getResource("/com/example/Interface.fxml"));
+            Scene scene = new Scene(root);
+            scene.setFill(javafx.scene.paint.Color.TRANSPARENT);
+            stage.initStyle(StageStyle.TRANSPARENT);
+            stage.setScene(scene);
+            stage.show();
+        } catch (IOException e) {e.printStackTrace();}
     }
+
+    public static void main(String[] args) {launch(args);}
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,13 +1,11 @@
 module com.example {
-    requires javafx.controls;
+    requires transitive javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
-    requires transitive java.sql;
-    requires transitive javafx.graphics;
-    requires mysql.connector.j;
+    requires java.sql;
 
     opens com.example to javafx.fxml;
-    opens com.example.controller to javafx.fxml;
     exports com.example;
-    exports com.example.connection;
+    opens com.example.controller to javafx.fxml;
+    exports com.example.controller;
 }

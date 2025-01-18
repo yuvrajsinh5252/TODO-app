7fa94844951df621333cdc278f2c469529adaef2
Yuvrajsinh Gohil <yuvrajsinh476@gmail.com>
2025-01-18 11:56:08 +0530

    refactor: enhance DBConnection

--- src/main/java/com/example/connection/DBConnection.java
+++ src/main/java/com/example/connection/DBConnection.java
@@ -2,22 +2,46 @@ package com.example.connection;
 
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class DBConnection {
-    public Connection dataBaseLink;
+    private Connection databaseLink;
+    private static final String DATABASE_NAME = "sys";
+    private static final String DATABASE_USER = "root";
+    private static final String DATABASE_PASSWORD = "8)Kw(M%L34G9";
+    private static final String SERVER_TIMEZONE = "UTC";
 
-    public Connection getConnection() {
-        String databaseName = "sys";
-        String dataBaseUser = "root";
-        String dataBasePassword = "8)Kw(M%L34G9";
-        String url = "jdbc:mysql://localhost:3306/" + databaseName;
-
-        try {
-            dataBaseLink = DriverManager.getConnection(url, dataBaseUser, dataBasePassword);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public Connection getConnection() throws SQLException {
+        if (databaseLink != null && !databaseLink.isClosed()) {
+            return databaseLink;
         }
 
-        return dataBaseLink;
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+
+            String url = String.format(
+                "jdbc:mysql://localhost:3306/%s?serverTimezone=%s&useSSL=false&allowPublicKeyRetrieval=true",
+                DATABASE_NAME,
+                SERVER_TIMEZONE
+            );
+
+            databaseLink = DriverManager.getConnection(url, DATABASE_USER, DATABASE_PASSWORD);
+            return databaseLink;
+
+        } catch (ClassNotFoundException e) {
+            throw new SQLException("MySQL JDBC Driver not found", e);
+        } catch (SQLException e) {
+            throw new SQLException("Failed to connect to database: " + e.getMessage(), e);
+        }
+    }
+
+    public void closeConnection() {
+        if (databaseLink != null) {
+            try {
+                databaseLink.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,11 +1,13 @@
 module com.example {
-    requires java.sql;
     requires javafx.controls;
     requires javafx.fxml;
     requires com.jfoenix;
+    requires transitive java.sql;
     requires transitive javafx.graphics;
+    requires mysql.connector.j;
 
     opens com.example to javafx.fxml;
     opens com.example.controller to javafx.fxml;
     exports com.example;
+    exports com.example.connection;
 }
